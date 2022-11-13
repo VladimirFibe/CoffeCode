4d0b7943ea6ba41f61b208a11de0570b1f1f7da8
Vladimir Fibe <motiw@icloud.com>
2022-11-13 23:36:49 +0600

    add LocationListView

---
+++ CoffeCode/CoffeCode/LocationListView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct LocationListView: View {
+    var body: some View {
+        Text("LocationList")
+    }
+}
+
+struct LocationListView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationListView()
+    }
+}
