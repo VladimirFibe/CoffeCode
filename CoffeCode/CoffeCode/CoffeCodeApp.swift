7d605c126bcbd795e787b3f113673072d10cf8b9
Vladimir Fibe <motiw@icloud.com>
2022-11-13 21:54:47 +0600

    start

---
+++ CoffeCode/CoffeCode/CoffeCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeCodeApp.swift
+//  CoffeCode
+//
+//  Created by Vladimir Fibe on 13.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoffeCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
